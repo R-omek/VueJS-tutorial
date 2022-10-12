694ab1b1be1eb4d46a7a40c24b045d89d1d66229
R-omek <rmngrck97@gmail.com>
2022-10-12 12:17:58 +0300

    use watch to watching value in input

--- app.js
+++ app.js
@@ -29,8 +29,15 @@ const App = {
     },
     computed: {
         doubleCountComputed() {
-            console.log('doubleCountComputed')
             return this.notes.length * 2
+        }
+    },
+    watch: {
+        inputValue(value) {
+            if(value.length > 10) {
+                this.inputValue = ''
+            }
+            console.log(value)
         }
     }
 }
