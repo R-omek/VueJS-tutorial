cab9002da3250009ba6042e4dabc1db523900024
R-omek <rmngrck97@gmail.com>
2022-10-12 12:13:49 +0300

    use labels for directives and computed property

--- app.js
+++ app.js
@@ -15,11 +15,22 @@ const App = {
             this.inputValue = event.target.value
         },
         addNewNotes() {
-            this.notes.push(this.inputValue)
-            this.inputValue = ''
+            if (this.inputValue !== '') {
+                this.notes.push(this.inputValue)
+                this.inputValue = ''
+            }
         },
         deleteNote(index) {
             this.notes.splice(index, 1)
+        },
+        toUpperCase(item) {
+            return item.toUpperCase()
+        }
+    },
+    computed: {
+        doubleCountComputed() {
+            console.log('doubleCountComputed')
+            return this.notes.length * 2
         }
     }
 }
